60e510af6237845e6809e77139cd0b3a0812d406
FAIZIC <165377072+FAIZIC@users.noreply.github.com>
2024-05-24 14:26:18 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class Main {
+    public static int[] fileReader(String file) throws FileNotFoundException {
+        Scanner scan = new Scanner(new File(file));
+        String line = scan.nextLine();
+        String[] sps2 = line.split(" ");
+        int[] sps = new int[sps2.length];
+        for (int i = 0; i < sps2.length; i++){
+            sps[i] = Integer.parseInt(sps2[i]);
+        }
+        return sps;
+    }
+    /*fileReader("test_zero.txt")*/
+    public static int _min(int[] sps){
+        int minim = sps[0];
+        for (int i = 1; i < sps.length; i++){
+            if (sps[i] < minim){
+                minim = sps[i];
+            }
+        }
+        return minim;
+    }
+
+    public static int _max(int[] sps){
+        int maxim = sps[0];
+        for (int i = 1; i < sps.length; i++){
+            if (sps[i] > maxim){
+                maxim = sps[i];
+            }
+        }
+        return maxim;
+    }
+    public static int _sum(int[] sps) {
+        int summ = sps[0];
+        for (int i = 1; i < sps.length; i++){
+            summ += sps[i];
+        }
+        return summ;
+    }
+
+    public static int _mult(int[] sps) {
+        int mult = sps[0];
+        for (int i = 1; i < sps.length; i++){
+            mult *= sps[i];
+        }
+        return mult;
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        org.junit.runner.JUnitCore.main("TestLoadFunction");
+        org.junit.runner.JUnitCore.main("TestFunction");
+    }
+}
---
+++ TestLoadFunction.java
@@ -0,0 +1,194 @@
+import org.junit.Test;
+import java.io.FileNotFoundException;
+
+public class TestLoadFunction {
+
+    public int[] sps;
+
+    public int[] massive_builder(String file){
+        try {
+            sps = Main.fileReader(file);
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден");
+            System.exit(1);
+        } catch (java.lang.NumberFormatException e) {
+            System.out.println("Выход за границы типа");
+            System.exit(1);
+        }
+        return sps;
+    }
+    public int[] sps_1 = massive_builder("src\\one_number.txt");
+    public int[] sps_100 = massive_builder("src\\100_numbers.txt");
+    public int[] sps_1000 = massive_builder("src\\1000_numbers.txt");
+    public int[] sps_10_000 = massive_builder("src\\10000_numbers.txt");
+    public int[] sps_100_000 = massive_builder("src\\100_000_numbers.txt");
+    public int[] sps_1_000_000 = massive_builder("src\\many_numbers.txt");
+
+    @Test(timeout = 10000)
+    public void loadSum_1(){
+        for (int i = 0; i < 10000; i++){
+            Main._sum(sps_1);
+        }
+    }
+
+    @Test(timeout = 10000)
+    public void loadSum_100(){
+        for (int i = 0; i < 10000; i++){
+            Main._sum(sps_100);
+        }
+    }
+
+    @Test(timeout = 10000)
+    public void loadSum_1000(){
+        for (int i = 0; i < 10000; i++){
+            Main._sum(sps_1000);
+        }
+    }
+
+    @Test(timeout = 10000)
+    public void loadSum_10_000(){
+        for (int i = 0; i < 10000; i++){
+            Main._sum(sps_10_000);
+        }
+    }
+
+    @Test(timeout = 10000)
+    public void loadSum_100_000(){
+        for (int i = 0; i < 10000; i++){
+            Main._sum(sps_100_000);
+        }
+    }
+
+    @Test(timeout = 10000)
+    public void loadSum_1_000_000(){
+        for (int i = 0; i < 10000; i++){
+            Main._sum(sps_1_000_000);
+        }
+    }
+
+    @Test(timeout = 10000)
+    public void loadMult_1(){
+        for (int i = 0; i < 10000; i++){
+            Main._mult(sps_1);
+        }
+    }
+
+    @Test(timeout = 10000)
+    public void loadMult_100(){
+        for (int i = 0; i < 10000; i++){
+            Main._mult(sps_100);
+        }
+    }
+
+    @Test(timeout = 10000)
+    public void loadMult_1000(){
+        for (int i = 0; i < 10000; i++){
+            Main._mult(sps_1000);
+        }
+    }
+
+    @Test(timeout = 10000)
+    public void loadMult_10_000(){
+        for (int i = 0; i < 10000; i++){
+            Main._mult(sps_10_000);
+        }
+    }
+
+    @Test(timeout = 10000)
+    public void loadMult_100_000(){
+        for (int i = 0; i < 10000; i++){
+            Main._mult(sps_100_000);
+        }
+    }
+
+    @Test(timeout = 10000)
+    public void loadMult_1_000_000(){
+        for (int i = 0; i < 10000; i++){
+            Main._mult(sps_1_000_000);
+        }
+    }
+
+    @Test(timeout = 10000)
+    public void loadMax_1(){
+        for (int i = 0; i < 10000; i++){
+            Main._max(sps_1);
+        }
+    }
+
+    @Test(timeout = 10000)
+    public void loadMax_100(){
+        for (int i = 0; i < 10000; i++){
+            Main._max(sps_100);
+        }
+    }
+
+    @Test(timeout = 10000)
+    public void loadMax_1000(){
+        for (int i = 0; i < 10000; i++){
+            Main._max(sps_1000);
+        }
+    }
+
+    @Test(timeout = 10000)
+    public void loadMax_10_000(){
+        for (int i = 0; i < 10000; i++){
+            Main._max(sps_10_000);
+        }
+    }
+
+    @Test(timeout = 10000)
+    public void loadMax_100_000(){
+        for (int i = 0; i < 10000; i++){
+            Main._max(sps_100_000);
+        }
+    }
+
+    @Test(timeout = 10000)
+    public void loadMax_1_000_000(){
+        for (int i = 0; i < 10000; i++){
+            Main._max(sps_1_000_000);
+        }
+    }
+
+    @Test(timeout = 10000)
+    public void loadMin_1(){
+        for (int i = 0; i < 10000; i++){
+            Main._min(sps_1);
+        }
+    }
+
+    @Test(timeout = 10000)
+    public void loadMin_100(){
+        for (int i = 0; i < 10000; i++){
+            Main._min(sps_100);
+        }
+    }
+
+    @Test(timeout = 10000)
+    public void loadMin_1000(){
+        for (int i = 0; i < 10000; i++){
+            Main._min(sps_1000);
+        }
+    }
+
+    @Test(timeout = 10000)
+    public void loadMin_10_000(){
+        for (int i = 0; i < 10000; i++){
+            Main._min(sps_10_000);
+        }
+    }
+
+    @Test(timeout = 10000)
+    public void loadMin_100_000(){
+        for (int i = 0; i < 10000; i++){
+            Main._min(sps_100_000);
+        }
+    }
+
+    @Test(timeout = 10000)
+    public void loadMin_1_000_000(){
+        for (int i = 0; i < 10000; i++){
+            Main._min(sps_1_000_000);
+        }
+    }
+}
